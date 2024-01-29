f82d7b9e18ce00ba9c6fea6d37875793b5cfdc72
Pavlos Smith <57727226+iByteABit256@users.noreply.github.com>
2024-01-29 17:22:08 +0200

    Added SaveItems capability to TODO items

--- todo/todo.go
+++ todo/todo.go
@@ -1,6 +1,22 @@
 package todo
 
+import (
+        "encoding/json"
+        "fmt"
+)
+
 type Item struct {
-    Text string
+        Text string
 }
 
+func SaveItems(filename string, items []Item) error {
+        b, err := json.Marshal(items)
+
+        if err != nil {
+                return err
+        }
+
+        fmt.Println(string(b))
+
+        return nil
+}
