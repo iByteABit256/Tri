888d8b0e5aa73f139618004cfee9433b7da97451
Pavlos Smith <57727226+iByteABit256@users.noreply.github.com>
2024-01-29 17:23:24 +0200

    Fixed errors and used SaveItems after adding

--- cmd/add.go
+++ cmd/add.go
@@ -5,41 +5,47 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
+        "fmt"
 
-	"github.com/spf13/cobra"
-	"github.com/iByteABit256/tri/todo"
+        "github.com/spf13/cobra"
+        "github.com/iByteABit256/tri/todo"
 )
 
 // addCmd represents the add command
 var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "Add a new Todo task",
-	Long: `Add will create a new Todo item in the Todo list`,
-	Run: addRun,
+        Use:   "add",
+        Short: "Add a new Todo task",
+        Long: `Add will create a new Todo item in the Todo list`,
+        Run: addRun,
 }
 
 func addRun(cmd *cobra.Command, args []string) {
     items := []todo.Item{}
 
     for _, x := range args {
-        items = append(todo.Item{Text:x})
-        fmt.Println("%v added to list.", x)
+        items = append(items, todo.Item{Text:x})
+        fmt.Printf("%v added to list.\n", x)
     }
 
-    fmt.Println("List: %v", items)
+    fmt.Printf("List: %#v\n", items)
+
+    filename := "my-todo-list.json"
+
+    todo.SaveItems(filename, items)
+
+    fmt.Printf("Saved to %v.", filename)
 }
 
 func init() {
-	rootCmd.AddCommand(addCmd)
+        rootCmd.AddCommand(addCmd)
 
-	// Here you will define your flags and configuration settings.
+        // Here you will define your flags and configuration settings.
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+        // Cobra supports Persistent Flags which will work for this command
+        // and all subcommands, e.g.:
+        // addCmd.PersistentFlags().String("foo", "", "A help for foo")
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+        // Cobra supports local flags which will only run when this command
+        // is called directly, e.g.:
+        // addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
