1c4572d22e6cfca2d8e8793bbe3ce59fcfb9ec5d
Pavlos Smith <57727226+iByteABit256@users.noreply.github.com>
2024-01-30 10:32:26 +0200

    Save items to file

--- todo/todo.go
+++ todo/todo.go
@@ -3,6 +3,7 @@ package todo
 import (
         "encoding/json"
         "fmt"
+        "io/ioutil"
 )
 
 type Item struct {
@@ -11,12 +12,14 @@ type Item struct {
 
 func SaveItems(filename string, items []Item) error {
         b, err := json.Marshal(items)
-
         if err != nil {
                 return err
         }
 
-        fmt.Println(string(b))
+        err = ioutil.WriteFile(filename, b, 0644)
+        if err != nil {
+                return err
+        }
 
         return nil
 }
